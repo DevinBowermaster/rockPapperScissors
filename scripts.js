e9d8f22d6ba163d8abdead3cd6ca9a3eacbc3af5
DevinBowermaster <mrdevinbowermaster@gmail.com>
2025-02-09 12:41:45 -0700

    Create play game and play round functions

--- scripts.js
+++ scripts.js
@@ -2,11 +2,11 @@
 A function needs to return a random string of rock paper or scissor
 */
 
-function computerChoice() {
+function getComputerChoice() {
     let x = Math.floor(Math.random() * 3)
 
     if (x === 0) {
-        return "Rock"
+        return "rock"
     } else if (x === 1) {
         return "paper"
     } else if (x === 2) {
@@ -15,7 +15,7 @@ function computerChoice() {
         return "error"
     }
 }
-console.log(computerChoice())
+
 
 /* Pseudocdoe
 A prompt needs to be created to have user choose rock paper or scissor 
@@ -34,4 +34,82 @@ function getHumanChoice() {
 }
 
 
-console.log(getHumanChoice())
+
+
+/* Pseudocode
+Create two variables for user and computer score
+*/
+
+
+
+/*Psuedocode
+Have the game start 
+get each players selection 
+Evaluate against each other rock beats scissors, paper beats rock, scissors beats paper.
+If selections are the same, game starts over
+have the winner announced
+increase the score of the winner
+*/
+
+
+let humanScore = 0;
+let computerScore = 0;
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+function playGame() {
+
+    playRound(computerSelection, humanSelection);
+
+
+
+    function playRound(computerChoice, humanChoice) {
+        if (computerChoice == "rock") {
+            if (humanChoice == "scissor") {
+                computerScore++
+                return "Computer Wins"
+            } else {
+                humanScore++
+                return "Player Wins"
+
+
+            }
+
+        } else if (computerChoice == "paper") {
+            if (humanChoice == "rock") {
+                computerScore++
+                return "Computer Wins"
+            } else {
+                humanScore++
+                return "Player Wins"
+            }
+
+        } else if (computerChoice == "scissor") {
+            if (humanChoice == "paper") {
+                computerScore++
+                return "Computer Wins"
+            } else {
+                humanScore++
+                return "Player Wins"
+            }
+        } else {
+
+        }
+    }
+
+    if (computerScore == 5) {
+        console.log("Computer Wins")
+    } else if (humanScore == 5) {
+        console.log("Player Wins")
+    } else {
+        anotherRound()
+    }
+}
+
+playGame()
+
+function anotherRound() {
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playGame()
+}
